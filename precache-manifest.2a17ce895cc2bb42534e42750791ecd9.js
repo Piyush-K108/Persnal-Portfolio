f6fde521902b25d73b9bf51fdf8842811ba46c7b
Prashant Khanchandani <gwar619908@gmail.com>
2023-03-05 21:13:57 +0530

    Updates

---
+++ precache-manifest.2a17ce895cc2bb42534e42750791ecd9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "458ef0fdd328046c501b",
+    "url": "/Persnal-Portfolio/static/css/main.02615fdf.chunk.css"
+  },
+  {
+    "revision": "458ef0fdd328046c501b",
+    "url": "/Persnal-Portfolio/static/js/main.3bb2c84b.chunk.js"
+  },
+  {
+    "revision": "97c7e81b1173a62d6027",
+    "url": "/Persnal-Portfolio/static/js/runtime~main.05386f55.js"
+  },
+  {
+    "revision": "e2331ff7884c2825cf45",
+    "url": "/Persnal-Portfolio/static/css/2.8ecbc69d.chunk.css"
+  },
+  {
+    "revision": "e2331ff7884c2825cf45",
+    "url": "/Persnal-Portfolio/static/js/2.af1e1005.chunk.js"
+  },
+  {
+    "revision": "78c46219922c2670ff5a",
+    "url": "/Persnal-Portfolio/static/js/3.b413865b.chunk.js"
+  },
+  {
+    "revision": "36850dffe5bc11d229a94a88db491d51",
+    "url": "/Persnal-Portfolio/static/media/proj1.36850dff.jpg"
+  },
+  {
+    "revision": "4154b70242188331e5c0fc30a1bb9a35",
+    "url": "/Persnal-Portfolio/static/media/proj2.4154b702.jpg"
+  },
+  {
+    "revision": "fc8028c73c6eb2ba98cc5689d7e07183",
+    "url": "/Persnal-Portfolio/static/media/proj3.fc8028c7.webp"
+  },
+  {
+    "revision": "8e03f996028d702e9874d3fc41bc6c47",
+    "url": "/Persnal-Portfolio/static/media/proj4.8e03f996.webp"
+  },
+  {
+    "revision": "d58760932862e9268ce2872f3ff4cc54",
+    "url": "/Persnal-Portfolio/index.html"
+  }
+];
