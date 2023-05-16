bc51206668b9363d9aed792534e29640bd0bcc94
Prashant Khanchandani <gwar619908@gmail.com>
2023-05-16 19:18:42 +0530

    3

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -11,53 +11,58 @@ function Experience() {
   return (
     <div className="experience">
       <VerticalTimeline lineColor="#3e497a">
-  
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="Feb 2022 - May 2022"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            My Cool University, Vancouver, British Columbia
-          </h3>
-
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor's Degree
-          </h4>
-
-          <p> Computer Science</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+      <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2018 - 2020"
+          date="2020 - Present"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Back End Engineer - Google
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>Developed the backend infrastructure for 3 projects.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2020 - present"
-          iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Full Stack Engineer - Twitch
+          Chameli Devi Group of Institutions 
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
+            Indore, India
           </h4>
+          <p> Computer Science And Engineering</p>
+        </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Feb 2022 - May 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            TechFloric, Indore, India
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Python Traning
+          </h4>
+
+          <p> Training in Python at TechFloric was awesome and that was my
+              first composer to how the Software Industry works and what the
+              chain of command looks like with the great environment
+              </p>
+        </VerticalTimelineElement>
+
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2017-2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+      <h3 className="vertical-timeline-element-title">
+          Shri Agrasen Vidyalaya
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+          High School
+          </h4>
+     
           <p>
-            Helped the team launch 2 major features by working both in the front
-            end and back end.
+          My school life was surely a learning experience. The confidence and motivation it gave me, I couldn't get anywhere else. Most importantly, it is the place where I recognized my uniqueness and individuality.
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
