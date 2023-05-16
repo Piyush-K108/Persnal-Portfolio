bcc5610c5f4541b8afb7b55fe95ba1e1593fedc3
Prashant Khanchandani <gwar619908@gmail.com>
2023-05-16 18:11:49 +0530

    2

---
+++ First/precache-manifest.3c692185271258691805e8533b3f7c1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cedd9cca89eb4e333a2",
+    "url": "/static/css/main.165b8acf.chunk.css"
+  },
+  {
+    "revision": "8cedd9cca89eb4e333a2",
+    "url": "/static/js/main.8cedd9cc.chunk.js"
+  },
+  {
+    "revision": "f9a280cce4a84bd8f31a",
+    "url": "/static/js/1.f9a280cc.chunk.js"
+  },
+  {
+    "revision": "44ba07b8c2d95056c4a2",
+    "url": "/static/css/2.ecc9704d.chunk.css"
+  },
+  {
+    "revision": "44ba07b8c2d95056c4a2",
+    "url": "/static/js/2.44ba07b8.chunk.js"
+  },
+  {
+    "revision": "148dedf133217d189ab6",
+    "url": "/static/js/runtime~main.148dedf1.js"
+  },
+  {
+    "revision": "07ec81cc36582083dc57cd59f924396c",
+    "url": "/static/media/logo.07ec81cc.jpg"
+  },
+  {
+    "revision": "4154b70242188331e5c0fc30a1bb9a35",
+    "url": "/static/media/proj2.4154b702.jpg"
+  },
+  {
+    "revision": "fc8028c73c6eb2ba98cc5689d7e07183",
+    "url": "/static/media/proj3.fc8028c7.webp"
+  },
+  {
+    "revision": "39b021a3c06803d9d436a5bb1dc07489",
+    "url": "/static/media/proj4.39b021a3.png"
+  },
+  {
+    "revision": "77bc0290f21ccca41754f4c809323f37",
+    "url": "/index.html"
+  }
+];
